b8eb9432cfd234684d93f270ac27d7ea62d8355d
way0utwest <sjones@sqlservercentral.com>
2014-05-12 14:18:07 -0600

    Debugging and restore checks
    Added debugging support in a few places and also changed logic to
    determine which databases are to be restored and which not

--- attachdbs.ps1
+++ attachdbs.ps1
@@ -4,6 +4,9 @@ Description: Attach databases from a folder that are not already on the instance
 
 $folder = 'D:\mssqlserver\MSSQL11.MSSQLSERVER\MSSQL\DATA'
 $instance = 'Tiny'
+$debug = 0
+$dbsexist = ""
+$dbsnotexist = ""
 
 #placeholder
 "================================================="
@@ -16,28 +19,59 @@ $instance = 'Tiny'
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoEnum") | Out-Null
 
 $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") $instance
-"Database List"
-"-------------"
+if ($debug -eq 1)
+ {
+    "Database List"
+    "-------------"
+    foreach($sqlDatabase in $Server.databases) 
+        { "DB:" + $sqlDatabase.name
+        }
+ }
 
-foreach($sqlDatabase in $Server.databases) 
-    { "DB:" + $sqlDatabase.name
-    }
+Write-Host "Checking Files"
 
 foreach ($file in Get-ChildItem $folder)
 {
-#$file.FullName
-#$file.Extension
+#Debug
+if ($debug -eq 1)
+    {
+     write-host $file.FullName
+     write-host $file.Extension
+     }
 
 if ($file.Extension -eq '.mdf') 
-  {"File:" + $file.BaseName
+  {
+    # output file name being checked
+    "File:" + $file.BaseName
+
+    # reset the check
+    $found = 0
+
+    # loop through each  of the databases
     foreach($sqlDatabase in $Server.databases) 
         { 
-
-          if ($file.BaseName -neq $sqlDatabase.name)
-            { "  **** Database Does Not Exist **** "
+          # check the file v the database name
+          if ($debug -eq 1)
+           {
+              "Checking " + $file.BaseName + " v " + $sqlDatabase.Name
             }
+
+          if ($file.BaseName -eq $sqlDatabase.name)
+            { 
+             $found = 1
+             $dbsexist = $dbsexist + $file.BaseName + ", "
+            }
+
         }
+    if ($found -eq 0)
+    {
+        "  **** Database Does Not Exist **** "
+        $dbsnotexist = $dbsnotexist + $file.BaseName + ", "
+    }
 
    }
 }
+write-host "Databases that exist: " + $dbsexist
+write-host "Databases that don't exist: " + $dbsnotexist
+write-host " "
 write-host "Done"
