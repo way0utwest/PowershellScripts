efcb515bb4ee733b240ec925005ca50992c5b13b
way0utwest <sjones@sqlservercentral.com>
2014-05-13 19:48:21 -0600

    Loop through all files - Initial commit
    File that finds all MDF files in a folder.

---
+++ LoopFiles.ps1
@@ -0,0 +1,28 @@
+﻿$folder = 'D:\mssqlserver\MSSQL11.MSSQLSERVER\MSSQL\DATA'
+$debug = 0
+
+# loop through each  of the file
+foreach ($file in Get-ChildItem $folder)
+{
+ #Debug
+ if ($debug -eq 1)
+    {
+     write-host "All files: " $file.name
+     #end debug
+    }
+
+ if ($file.Extension -eq '.mdf') 
+    {
+     if ($debug -eq 1)
+     {
+      write-host "MDF Files: "  $file.name
+      #end debug
+     }
+
+    $file.Name
+
+    # end if
+    }
+
+# end for loop of files
+}
